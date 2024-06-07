b63c1ad32f40e68b45163a1ee0ac1c6d67c3aa1d
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-06-07 11:17:31 +0700

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Candidate College Careers",
+  description: "Join the team at Candidate College Careers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
