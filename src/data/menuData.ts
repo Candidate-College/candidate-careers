4692c30523bdaa4d3baec4f6a0baa6e6ddda3bff
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-06-07 18:30:38 +0700

    Refactor menuData.ts to use absolute URLs for links

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -1,12 +1,16 @@
 export const menus = [
-    { id: 1, name: 'Home', link: '/', },
-    { id: 2, name: 'About Us', link: '/about', },
-    { id: 4, name: 'Articles', link: '/articles', },
-    { id: 5, name: 'Events', link: '/events', },
-]
+  { id: 1, name: "Home", link: "https://candidatecollege.org/" },
+  { id: 2, name: "About Us", link: "https://candidatecollege.org/about" },
+  { id: 4, name: "Articles", link: "https://candidatecollege.org/articles" },
+  { id: 5, name: "Events", link: "https://candidatecollege.org/events" },
+];
 
 export const footers = [
-    { id: 1, name: 'Privacy Policy', link: '/', },
-    { id: 2, name: 'Terms & Conditions', link: '/about', },
-    { id: 3, name: 'Cookie Policy', link: '', },
-]
+  { id: 1, name: "Privacy Policy", link: "https://candidatecollege.org/" },
+  {
+    id: 2,
+    name: "Terms & Conditions",
+    link: "https://candidatecollege.org/about",
+  },
+  { id: 3, name: "Cookie Policy", link: "" },
+];
