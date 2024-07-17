85052f0de12fcc6b2b28dcd7f98c1c5b3ba519b8
zidanindratama <zidanindratama31@gmail.com>
2024-07-17 11:17:08 +0700

    feat: display dynamic data on form page and submit it to the backend api

--- src/components/form-te/QuestionForm.tsx
+++ src/components/form-te/QuestionForm.tsx
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import LoadingBar from 'react-top-loading-bar';
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import LoadingBar from "react-top-loading-bar";
+import useFormStore from "@/components/store/formStore"; // Adjust path as per your project
+import TitleForm from "./TitleForm";
 
 const QuestionForm: React.FC = () => {
   const router = useRouter();
-  const [formData, setFormData] = useState({
-    skills: '',
-    schedule: '',
-    commitment: '',
-    commitmentReason: '',
-  });
+  const { formData, setFormData } = useFormStore();
   const [progress, setProgress] = useState(0);
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const [navigated, setNavigated] = useState(false);
@@ -23,19 +20,24 @@ const QuestionForm: React.FC = () => {
     }
   }, [navigated]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-    setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
   };
 
   const validateForm = () => {
     let newErrors: { [key: string]: string } = {};
 
-    if (!formData.skills) newErrors.skills = 'Skills are required';
-    if (!formData.schedule) newErrors.schedule = 'Schedule is required';
-    if (!formData.commitment) newErrors.commitment = 'Commitment is required';
-    if (!formData.commitmentReason) newErrors.commitmentReason = 'Commitment reason is required';
+    if (!formData.skills) newErrors.skills = "Skills are required";
+    if (!formData.schedule) newErrors.schedule = "Schedule is required";
+    if (!formData.commitment) newErrors.commitment = "Commitment is required";
+    if (!formData.commitmentReason)
+      newErrors.commitmentReason = "Commitment reason is required";
 
     setErrors(newErrors);
 
@@ -45,9 +47,9 @@ const QuestionForm: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (validateForm()) {
-      setProgress(100); 
+      setProgress(100);
       setNavigated(true);
-      await router.push('/apply-form/uploadpage/questionpage/finalpage');
+      await router.push("/apply-form/uploadpage/questionpage/finalpage");
     }
   };
 
@@ -57,7 +59,7 @@ const QuestionForm: React.FC = () => {
         color="#FFDE59"
         progress={progress}
         height={10}
-        className='rounded-full'
+        className="rounded-full"
         onLoaderFinished={() => setProgress(0)}
       />
       <div className="container mx-auto px-4 py-2 md:py-16 max-w-screen-lg xxsm:mt-32">
@@ -71,17 +73,21 @@ const QuestionForm: React.FC = () => {
               className="max-w-full h-auto"
             />
           </div>
-          <h2 className="text-xl md:text-2xl font-bold mb-2 md:mb-4 text-center text-slate-700 sm:text-lg">Front-End Developer</h2>
-          <div className="flex flex-wrap space-x-2 mb-4 md:mb-6 justify-center">
-            <span className="bg-amber-300 text-sm font-medium px-2 md:px-3 py-1 rounded-full text-stone-600 cursor-pointer">Internship</span>
-            <span className="bg-amber-300 text-sm font-medium px-2 md:px-3 py-1 rounded-full text-stone-600 cursor-pointer">Staff</span>
-            <span className="bg-red-600 text-sm font-medium px-2 md:px-3 py-1 rounded-full text-slate-200 cursor-pointer">Urgent Hiring</span>
-          </div>
+          {/* REPLACE WITH YOUR TITLE COMPONENT */}
+          <TitleForm />
           <form onSubmit={handleSubmit}>
-            <h3 className="text-lg md:text-xl font-bold mb-4 md:mb-6 text-slate-700 sm:text-base mt-12">Fill in your answer for the following questions</h3>
+            <h3 className="text-lg md:text-xl font-bold mb-4 md:mb-6 text-slate-700 sm:text-base mt-12">
+              Fill in your answer for the following questions
+            </h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
               <div>
-                <label htmlFor="skills" className="block text-sm font-medium text-slate-700 mb-1 md:mb-2">Describe your skills and experiences that related with this position?<span className="text-red-500">*</span></label>
+                <label
+                  htmlFor="skills"
+                  className="block text-sm font-medium text-slate-700 mb-1 md:mb-2"
+                >
+                  Describe your skills and experiences that related with this
+                  position?<span className="text-red-500">*</span>
+                </label>
                 <textarea
                   id="skills"
                   name="skills"
@@ -91,9 +97,18 @@ const QuestionForm: React.FC = () => {
                   className="w-full h-28 px-2 py-2 md:px-3 md:py-3 text-sm text-slate-700 bg-slate-50 border border-slate-600 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 placeholder-slate-400 resize-none"
                   required
                 />
+                {errors.skills && (
+                  <p className="text-red-500 text-sm">{errors.skills}</p>
+                )}
               </div>
               <div>
-                <label htmlFor="schedule" className="block text-sm font-medium text-slate-700 mb-1 md:mb-2">What you&apos;ll be busy with and what your schedule for the next few months?<span className="text-red-500">*</span></label>
+                <label
+                  htmlFor="schedule"
+                  className="block text-sm font-medium text-slate-700 mb-1 md:mb-2"
+                >
+                  What you&apos;ll be busy with and what your schedule for the
+                  next few months?<span className="text-red-500">*</span>
+                </label>
                 <textarea
                   id="schedule"
                   name="schedule"
@@ -103,14 +118,26 @@ const QuestionForm: React.FC = () => {
                   className="w-full h-28 px-2 py-2 md:px-3 md:py-3 text-sm text-slate-700 bg-slate-50 border border-slate-600 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 placeholder-slate-400 resize-none"
                   required
                 />
+                {errors.schedule && (
+                  <p className="text-red-500 text-sm">{errors.schedule}</p>
+                )}
               </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
               <div>
-                <label htmlFor="commitment" className="block text-sm font-medium text-slate-700 mb-1 md:mb-2">How committed are you to internship at Candidate College?<span className="text-red-500">*</span></label>
+                <label
+                  htmlFor="commitment"
+                  className="block text-sm font-medium text-slate-700 mb-1 md:mb-2"
+                >
+                  How committed are you to internship at Candidate College?
+                  <span className="text-red-500">*</span>
+                </label>
                 <div className="flex justify-between text-slate-500 p-2">
                   {[1, 2, 3, 4, 5].map((num) => (
-                    <label key={num} className="flex flex-col items-center w-full cursor-pointer text-center">
+                    <label
+                      key={num}
+                      className="flex flex-col items-center w-full cursor-pointer text-center"
+                    >
                       <input
                         type="radio"
                         name="commitment"
@@ -119,13 +146,30 @@ const QuestionForm: React.FC = () => {
                         onChange={handleChange}
                         className="hidden"
                       />
-                      <span className={`w-full py-3 border border-slate-500 rounded-sm ${formData.commitment === num.toString() ? 'bg-amber-300 text-slate-700' : 'bg-zinc-100 text-gray-700'}`}>{num}</span>
+                      <span
+                        className={`w-full py-3 border border-slate-500 rounded-sm ${
+                          formData.commitment === num.toString()
+                            ? "bg-amber-300 text-slate-700"
+                            : "bg-zinc-100 text-gray-700"
+                        }`}
+                      >
+                        {num}
+                      </span>
                     </label>
                   ))}
                 </div>
+                {errors.commitment && (
+                  <p className="text-red-500 text-sm">{errors.commitment}</p>
+                )}
               </div>
               <div>
-                <label htmlFor="commitmentReason" className="block text-sm font-medium text-slate-700 mb-1 md:mb-2">According to the number you chose, give your reasons for rating your commitment<span className="text-red-500">*</span></label>
+                <label
+                  htmlFor="commitmentReason"
+                  className="block text-sm font-medium text-slate-700 mb-1 md:mb-2"
+                >
+                  According to the number you chose, give your reasons for
+                  rating your commitment<span className="text-red-500">*</span>
+                </label>
                 <textarea
                   id="commitmentReason"
                   name="commitmentReason"
@@ -135,11 +179,27 @@ const QuestionForm: React.FC = () => {
                   className="w-full h-28 px-2 py-2 md:px-3 md:py-3 text-sm text-slate-700 bg-slate-50 border border-slate-600 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 placeholder-slate-400 resize-none"
                   required
                 />
+                {errors.commitmentReason && (
+                  <p className="text-red-500 text-sm">
+                    {errors.commitmentReason}
+                  </p>
+                )}
               </div>
             </div>
             <div className="flex justify-between sm:px-4">
-              <button type="button" onClick={() => router.push('/apply-form/uploadpage')} className="w-28 bg-transparent border text-slate-950 opacity-75 p-4 shadow-lg text-sm rounded-full hover:bg-primary hover:text-amber-300 transition duration-300 font-semibold xxsm:mt-4">Previous</button>
-              <button type="submit" className="w-28 bg-amber-400 text-slate-950 opacity-75 p-4 shadow-lg text-sm rounded-full hover:bg-yellow-600 transition duration-300 font-semibold xxsm:mt-4">Submit</button>
+              <button
+                type="button"
+                onClick={() => router.push("/apply-form/uploadpage")}
+                className="w-28 bg-transparent border text-slate-950 opacity-75 p-4 shadow-lg text-sm rounded-full hover:bg-primary hover:text-amber-300 transition duration-300 font-semibold xxsm:mt-4"
+              >
+                Previous
+              </button>
+              <button
+                type="submit"
+                className="w-28 bg-amber-400 text-slate-950 opacity-75 p-4 shadow-lg text-sm rounded-full hover:bg-yellow-600 transition duration-300 font-semibold xxsm:mt-4"
+              >
+                Submit
+              </button>
             </div>
           </form>
         </div>
