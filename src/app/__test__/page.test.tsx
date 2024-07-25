8e81cd7b2c431406ea6625905160219793146d87
ashafaatadhis <skullreaper387@gmail.com>
2024-07-25 14:04:16 +0700

    feat: setup testing with jest

---
+++ src/app/__test__/page.test.tsx
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Page from "../page";
+
+describe("Page", () => {
+  it("to be true", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
