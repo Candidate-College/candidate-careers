b3586c5a0cf40067722ad0b16ce1722244b248c4
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2025-06-28 13:10:15 +0700

    fix imports

--- src/app/all-jobs/[slug]/form/page.tsx
+++ src/app/all-jobs/[slug]/form/page.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Image from "next/image";
 export default function Page() {
   return (
     <div
